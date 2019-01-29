16b84fb5dfe4133a94dbb8a6ad84cd5eccdf8872
邓剑波 <dengjianbo@live.cn>
2019-01-29 11:34:36 +0800

    yolov3-20190129

--- detect.py
+++ detect.py
@@ -23,4 +23,52 @@ def arg_parse():
     parser.add_argument("--images", dest = "images", help =
                         "Image /Directory containing images to perform detection upon",
                         default = "imgs", type = str)
-    parser.add_argument("--det", )
+    parser.add_argument("--det", dest = 'det', help =
+                        "Image / Directory to store detections to",
+                        default = "det", type = str)
+    parser.add_argument("--bs", dest = "bs", help = "Batch size", default = 1)
+    parser.add_argument("--confidence", dest = "confidence", help =
+                        "Object Confidence to filter predictions", default = 0.5)
+    parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
+    parser.add_argument("--cfg", dest = "cfgfile", help = "weightsfile",
+                        default = "yolov3.weights", type = str)
+    parser.add_argument("--reso", dest = "reso", help =
+                        "Input resolution of the network. Increase to increase accuracy. Decrese to increase speed",
+                        default = "416", type = str)
+
+    return parser.parse_args()
+
+args = arg_parse()
+images = args.images
+batch_size = int(args.bs)
+confidence = float(args.confidence)
+nms_thresh = float(args.nms_thresh)
+start = 0
+CUDA = torch.cuda.is_available()
+
+
+num_classes = 80
+classes = load_classes("data/coco.names")
+
+#set up the neural network
+print("Loading network .....")
+model = Darknet(args.cfgfile)
+model.load_weights(args.weightsfile)
+print("Network successfully loaded")
+
+
+model.net_info["height"] = args.reso
+inp_dim = int(model.net_info["height"])
+assert inp_dim % 32 == 0
+assert inp_dim > 32
+
+if CUDA:
+    model.cuda()
+
+
+#set the model in evaluation mode
+model.eval()
+
+read_dir = time.time()
+
+#
---
+++ util.py
@@ -0,0 +1,57 @@
+from __future__ import division
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import cv2
+
+def unique(tensor):
+    tensor_np = tensor.cpu().numpy()
+    unique_np = np.unique(tensor_np)
+    unique_tensor = torch.from_numpy(unique_np)
+
+    tensor_res = tensor.new(unique_tensor.shape)
+    tensor_res.copy_(unique_tensor)
+    return tensor_res
+
+
+def bbox_iou(box1, box2):
+    """
+    return the IOU of two bounding boxes
+    :param box1:
+    :param box2:
+    :return:
+    """
+    #getting coordinates of bounding boxes
+    b1_x1, b1_y1, b1_x2, b1_y2 = box1[:,0], box1[:,1], box1[:,2], box1[:,3]
+    b2_x1, b2_y2, b2_x2, b2_y2 = box2[:,0], box2[:,1], box2[:,2], box2[:,3]
+
+    #getting coordinates of intersection rectangle
+    inter_rect_x1 = torch.max(b1_x1, b2_x1)
+    inter_rect_y1 = torch.max(b1_y1, b2_y1)
+    inter_rect_x2 = torch.min(b1_x2, b2_x2)
+    inter_rect_y2 = torch.min(b1_y2, b2_y2)
+
+    #intersection area
+    inter_area = torch.clamp(inter_rect_x2 - inter_rect_x1 + 1, min=0) * torch.clamp(inter_rect_y2 - inter_rect_y1 + 1, min = 0)
+
+    #union area
+    b1_area = (b1_x2 - b1_x1 + 1)*(b1_y2 - b1_y1 + 1)
+    b2_area = (b2_x2 - b2_x1 + 1)*(b2_y2 - b2_y1 + 1)
+
+    iou = inter_area / (b1_area + b2_area - inter_area)
+
+    return iou
+
+def predict_transform(prediction, inp_dim, anchors, num_classes, CUDA = True):
+
+    batch_size = prediction.size(0)
+    stride = inp_dim // prediction.size(2)
+    grid_size = inp_dim // stride
+    bbox_attrs = 5 + num_classes
+    num_anchors = len(anchors)
+
+    prediction = prediction.view(batch_size, bbox_attrs*num_anchors, grid_size*grid_size)
+    prediction = prediction.transpose(1,2).contiguous()
