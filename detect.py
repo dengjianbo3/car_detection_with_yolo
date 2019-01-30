5ca9b6f2efdda87695d69c8f366cd58aab58ed67
邓剑波 <dengjianbo@live.cn>
2019-01-30 11:36:10 +0800

    yolov3-20190130

--- detect.py
+++ detect.py
@@ -65,10 +65,110 @@ assert inp_dim > 32
 if CUDA:
     model.cuda()
 
-
 #set the model in evaluation mode
-model.eval()
+mode.eval()
 
 read_dir = time.time()
 
-#
+#detection phase
+try:
+    imlist = [osp.join(osp.ralpath(','), images, img) for img in os.listdir(images)]
+except NotADirectoryError:
+    imlist = []
+    imlist.append(osp.join(osp.realpath(','), images))
+except:
+    print("No file or directory with the name {}".format(images))
+    exit()
+
+if not os.path.exists(args.det):
+    os.makedirs(args.det)
+
+load_batch = time.time()
+loaded_ims = [cv2.imread(x) for x in imlist]
+
+im_batches = list(map(prep_image, loaded_ims, [inp_dim for x in range(len(imlist))]))
+im_dim_list = [(x.shape[1], x.shape[0]) for x in loaded_ims]
+im_dim_list = torch.FloatTensor(im_dim_list).repeat(1,2)
+
+leftover = 0
+if (len(im_dim_list) % batch_size):
+    leftover = 1
+
+if batch_size != 1:
+    num_batches = len(imlist) // batch_size + leftover
+    im_batches = [torch.cat((im_batches[i*batch_size : min((i + 1)*batch_size,
+                                                           len(im_batches))])) for i in range(num_batchs)]
+
+write = 0
+
+if CUDA :
+    im_dim_list = im_dim_list.cuda()
+
+start_det_loop = time.time()
+for i,batch in enumerate(im_batches):
+    #load the image
+    start = time.time()
+    if CUDA:
+        batch = batch.cuda()
+    with torch.no_grad():
+        prediction = mode(Variable(batch), CUDA)
+
+    prediction = write_results(prediction, confidence, num_classes, nms_conf=nms_thesh)
+
+    end = time.time()
+
+    if type(prediction) == int:
+
+        for im_num, image in enumerate(imlist[i*batch_size: min((i+1)*batch_size, len(imlist))]):
+            im_id = i*batch_size + im_num
+            print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+            print("{0:20s} {1:s}".format("Object Detected:", ""))
+            print("-------------------------------------------------------")
+
+        continue
+
+    prediction[:,0] += i*batch_size
+
+    if not write:
+        output = prediction
+        write = 1
+    else:
+        output = torch.cat((output, prediction))
+
+    for im_num, image in enumerate(imlist[i*batch_size : ming((i+1)*batch_size, len(imlist))]):
+        im_id = i*batch_size + im_num
+        objs = [classes[int(x[-1])] for x in output if int(x[0]) == im_id]
+        print("{0:20s} predicted in {1:6.3f} seconds".format(image.split("/")[-1], (end - start)/batch_size))
+        print("{o:20s} {1:s}".format("Objects Dectected:"," ".join(objs)))
+        print("------------------------------------------------------------")
+
+    if CUDA:
+        torch.cuda.synchronize()
+
+
+try:
+    output
+except NameError:
+    print("No detections were made")
+    exit()
+
+im_dim_list = torch.index_select(im_dim_list, 0, output[:,0].long())
+
+scaling_factor = torch.min(416/im_dim_list, 1)[0].view(-1,1)
+
+output[:, [1,3]] -= (inp_dim - scaling_factor*im_dim_list[:,0].view(-1,1))/2
+otuput[:, [2,4]] -= (inp_dim - scaling_factor*im_dim_list[:,1].view(-1,1))/2
+
+output[:,1:5] /= scaling_factor
+
+for i in range(output.shape[0]):
+    output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim_list[i,0])
+    output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim_list[i,1])
+
+output_recast = time.time()
+class_load = time.time()
+colors = okl.load(open("pallete","rb"))
+
+draw = time.time()
+
+def write(x, results):
